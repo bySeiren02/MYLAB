ab1e7e5ddddbb806417495ab4a327c78eafd6ab4
yoomin.lee <yoomin.lee@lemonhc.com>
2026-04-22 13:34:01 +0900

    [MYLAB] - CLEAR

--- backend/src/main/java/com/basiccrud/exception/DuplicateEmailException.java
+++
@@ -1,8 +0,0 @@
-package com.basiccrud.exception;
-
-public class DuplicateEmailException extends RuntimeException {
-
-    public DuplicateEmailException(String message) {
-        super(message);
-    }
-}
